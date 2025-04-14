4901f146ea58e4544ef634a2e58832d418e3153e
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-04-14 06:42:36 -0600

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if ((!Number.isInteger(first)) || (!Number.isInteger(second))) {
+        return "ERROR";
+    }
 
+    if (first < 0 || second < 0) {
+        return "ERROR";
+    }
+
+    if (first > second) {
+        lower = second;
+        higher = first;
+    }
+
+    else {
+        lower = first;
+        higher = second;
+    }
+
+    let sum = 0;
+    for (let i = lower; i <= higher; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
