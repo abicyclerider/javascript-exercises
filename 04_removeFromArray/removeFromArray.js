523064c94a82a44d4361519a316a6558031645c0
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-04-14 06:31:05 -0600

    Finish 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elements) {
+    for (i = 0; i < arr.length; i ++) {
+        for (j = 0; j < elements.length; j ++) {
+            if (arr[i] === elements[j]) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
