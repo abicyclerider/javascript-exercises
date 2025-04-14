855b74a3798b95be7deb5de73213fe9537373ee7
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-04-14 07:02:35 -0600

    finish 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    divisible4 = !(year%4);
+    divisible100 = !(year%100);
+    divisible400 = !(year%400)
 
+    return (divisible4 && (!divisible100 || divisible400))
 };
 
 // Do not edit below this line
