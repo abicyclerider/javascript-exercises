4bbe7755504eddeef3a5c9d93296ec173648b94b
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-04-15 06:19:44 -0600

    Finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue);
+  }
+  else {return 0;}
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = 1;
+  for (n of numbers) {
+    result *= n;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, power) {
+  return a ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {return 1;}
+  let result = number;
+  for (let i = number -1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
