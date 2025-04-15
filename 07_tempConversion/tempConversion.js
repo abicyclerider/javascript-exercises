cfe1deaf779fe6161086987d15dfed2c27c8432c
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-04-15 06:02:02 -0600

    finish 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFarenheit) {
+  let degreeCelcius = (degreeFarenheit - 32)*(5/9);
+  return Math.round(degreeCelcius*10)/10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelcius) {
+  let degreeFarenheit =  degreeCelcius*(9/5) + 32;
+  return Math.round(degreeFarenheit*10)/10;
 };
 
 // Do not edit below this line
