41b971f7afd8fc1026eb57fc3fffc81406a10938
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-04-15 06:47:43 -0600

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let noSpacesPunctuation = string.replace(/[^\w\d]/g, '').toLowerCase();
+    let reversed = noSpacesPunctuation.split('').reverse().join('');
 
+    return noSpacesPunctuation === reversed;
 };
 
 // Do not edit below this line
