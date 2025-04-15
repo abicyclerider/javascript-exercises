e9a93c321e20a6a6287876e18177bfdc8c18b194
Alex Rider <177359264+abicyclerider@users.noreply.github.com>
2025-04-15 07:14:00 -0600

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (!isNaN(Number(number))) {
+        number = Number(number)
+    }
+    if (number < 0) {return "OOPS";}
 
+    let fibonacciSequence = [0, 1, 1];
+
+    for (i = 3; i < number + 1; i++) {
+        fibonacciSequence.push(fibonacciSequence[i - 1] + fibonacciSequence[i - 2])
+    }
+
+    return fibonacciSequence[number];
 };
 
 // Do not edit below this line
